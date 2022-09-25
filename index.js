90aaa37c9950c66f8aed1e097b9b443128143fba
enisgjini <gjinienis148@gmail.com>
2022-09-25 18:38:09 +0200

    #

---
+++ index.js
@@ -0,0 +1,44 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 1, 2022 13:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML =
+    days +
+    " ditë " +
+    hours +
+    " orë " +
+    minutes +
+    " minuta " +
+    seconds +
+    " sekonda ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+$(document).on("click", 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top + -100,
+    },
+    1000
+  );
+});
